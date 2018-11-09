55f41519bb0c6786476bf8f550972030bfc336a5
johnhovanec <jahovanec@gmail.com>
2018-11-09 16:42:16 -0500

    Add order confirmation after checkout

--- bikestoreAPI/Controllers/OrdersController.cs
+++ bikestoreAPI/Controllers/OrdersController.cs
@@ -6,9 +6,11 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using bikestoreAPI.Models;
+using Microsoft.AspNetCore.Cors;
 
 namespace bikestoreAPI.Controllers
 {
+    [EnableCors("AllowMyOrigin")]
     [Produces("application/json")]
     [Route("api/Orders")]
     public class OrdersController : Controller
@@ -28,22 +30,71 @@ namespace bikestoreAPI.Controllers
         }
 
         // GET: api/Orders/5
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetOrder([FromRoute] int id)
+        [HttpGet("~/api/orders/confirmation")]
+        public IEnumerable<OrderItems> GetOrder([FromQuery] string Id)
         {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            var order = await _context.Order.SingleOrDefaultAsync(m => m.Id == id);
-
+            // Get the order
+            var order = _context.Order.SingleOrDefault(m => m.Id == Int32.Parse(Id));
             if (order == null)
-            {
-                return NotFound();
-            }
+                return Enumerable.Empty<OrderItems>();
 
-            return Ok(order);
+            var prods =
+                from op in _context.OrderProduct
+                join prod in _context.Product on op.ProductId equals prod.Id
+                where op.OrderId == order.Id
+                select new OrderItems
+                {
+                    Id = op.Id,
+                    CartId = (int)order.ShoppingCartId,
+                    Model = prod.Model,
+                    Manufacturer = prod.Manufacturer,
+                    Quantity = op.Quantity,
+                    InventoryQuantity = prod.InventoryQuantity,
+                    UnitPrice = op.UnitPrice,
+                    Color = op.Color,
+                    Size = op.Size,
+                    Subtotal = order.Subtotal,
+                    Shipping = order.ShippingCost,
+                    Tax = order.Tax,
+                    Total = order.Total
+                };
+
+            return (prods);
+            //var orderSummary = (from op in _context.OrderProduct
+            //                     join p in _context.Product on op.ProductId equals p.Id
+            //                     where op.OrderId == id
+            //                     select new OrderSummary()
+            //                     {
+            //                         Tax = order.Tax,
+            //                         SourceCode = order.SourceCode,
+            //                         ShippingAddressId = order.ShippingAddressId,
+            //                         ShippingCost = order.ShippingCost,
+            //                         Subtotal = order.Subtotal,
+            //                         Total = order.Total,
+            //                     }).ToList();
+
+            //if (order == null)
+            //{
+            //    return NotFound();
+            //}
+
+            //// Retrieve all products for the order
+            //var orderProducts = _context.OrderProduct.Where(p => p.OrderId == id);
+
+            //// Map results to new object
+            //var orderSummary = new OrderSummary()
+            //{
+            //    TimeStamp = order.TimeStamp,
+            //    Tax = order.Tax,
+            //    SourceCode = order.SourceCode,
+            //    ShippingAddressId = order.ShippingAddressId,
+            //    ShippingCost = order.ShippingCost,
+            //    Subtotal = order.Subtotal,
+            //    Total = order.Total,
+
+            //}
+
+            //return Ok(order);
         }
 
         // PUT: api/Orders/5
@@ -82,6 +133,7 @@ namespace bikestoreAPI.Controllers
         }
 
         // POST: api/Orders
+        [EnableCors("AllowMyOrigin")]
         [HttpPost]
         public async Task<IActionResult> PostOrder([FromBody] CartProduct productInCart)
         {
@@ -147,7 +199,7 @@ namespace bikestoreAPI.Controllers
                     throw;
                 }
 
-                return CreatedAtAction("GetOrder", new { id = order.Id }, order);
+                return Ok(order.Id);
             }
             return NotFound();
         }
@@ -199,6 +251,44 @@ namespace bikestoreAPI.Controllers
             public List<ShoppingCartProduct> cartProducts { get; set; }
             public string cartTotal { get; set; }
             public string sessionId { get; set; }
+        }
+
+
+        public class OrderSummary
+        {
+            public int Id { get; set; }
+            public DateTime? TimeStamp { get; set; }
+            public decimal Tax { get; set; }
+            public string SourceCode { get; set; }
+            public int ShippingAddressId { get; set; }
+            public decimal ShippingCost { get; set; }
+            public decimal Subtotal { get; set; }
+            public decimal Total { get; set; }
+            public List<Product> products { get; set; }
+            public int? Quantity { get; set; }
+            public string Size { get; set; }
+            public string Color { get; set; }
+            public decimal? UnitPrice { get; set; }
+        }
+
+
+        public class OrderItems
+        {
+            public int Id { get; set; }
+            public int CartId { get; set; }
+            public string Manufacturer { get; set; }
+            public string Model { get; set; }
+            public string Size { get; set; }
+            public string Color { get; set; }
+            public decimal Price { get; set; }
+            public int? InventoryQuantity { get; set; }
+            public int CartQuantity { get; set; }
+            public int? Quantity { get; set; }
+            public decimal? UnitPrice { get; set; }
+            public decimal Subtotal { get; set; }
+            public decimal Tax { get; set; }
+            public decimal Shipping { get; set; }
+            public decimal Total { get; set; }
         }
     }
 }
